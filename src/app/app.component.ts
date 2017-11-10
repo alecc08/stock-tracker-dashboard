aa5600c2e9e577b78175059428e9ed03a20f3e3b
alec <14881741+alecc08@users.noreply.github.com>
2017-11-09 21:29:53 -0500

    Start of account creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StockService } from './stock.service';
 
 @Component({
   selector: 'app-root',
@@ -7,21 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  selectedAccounts: any[];
-  selectedPortfolios: any[];
+  accounts: any[];
+  selectedPortfolio: any;
+  selectedAccount: any;
 
-  constructor() {
+  constructor(private stockService: StockService) {
 
   }
 
   ngOnInit() {
+    // Get accounts
+    this.stockService.getAccounts().then((accounts) => this.accounts = accounts)
+      .catch(this.handleError);
   }
 
   selectAccount() {
 
   }
   selectPortfolio() {
-    
+
+  }
+
+  handleError(err) {
+    console.log(err);
   }
 
 }
--- src/app/stock.service.ts
+++ src/app/stock.service.ts
@@ -4,20 +4,31 @@ import { Http } from '@angular/http';
 @Injectable()
 export class StockService {
 
-  apiUrl = 'http://localhost:8080/stocks?';
+  apiUrl = 'http://localhost:8080/';
   options = null;
 
   constructor(private http: Http) { }
 
   getStockData(stocks, start, end): Promise<any[]> {
-    console.log(this.apiUrl + `stockCodes=${stocks}&start=${start}&end=${end}`);
-    return this.http.get(this.apiUrl + `stockCodes=${stocks}&start=${start}&end=${end}`).toPromise()
+    return this.http.get(this.apiUrl + `stocks?stockCodes=${stocks}&start=${start}&end=${end}`).toPromise()
+    .then(response => response.json())
+    .catch(this.handleError);
+  }
+
+  getAccounts(): Promise<any[]> {
+    return this.http.get(this.apiUrl + 'accounts').toPromise()
+    .then(response => response.json())
+    .catch(this.handleError);
+  }
+
+  createAccount(accountName): Promise<any[]> {
+    return this.http.post(this.apiUrl + 'accounts', {accountName: accountName}).toPromise()
     .then(response => response.json())
     .catch(this.handleError);
   }
 
   private handleError(err) {
-    console.log('ERROR!' + err);
+    alert("ERROR: " + err);
   }
 
 }
